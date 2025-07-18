e483e8bf1f6294b2a17eab08c3ae5093d75d51b5
Henry He <henryhyn@163.com>
2025-07-18 15:15:56 +0800

    feat: v0.1.3

--- src/ultraflow/__init__.py
+++ src/ultraflow/__init__.py
@@ -1,5 +1,5 @@
 from ultraflow.core.flow import Prompty
 from ultraflow.core.start_flow import FlowProcessor
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __all__ = ['Prompty', 'FlowProcessor', '__version__']
